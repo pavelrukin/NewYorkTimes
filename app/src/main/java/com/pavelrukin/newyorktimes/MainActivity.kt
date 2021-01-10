0f778decba4a75ae4bdb3201876eb6cd47590bc3
Pavel Rukin <rivelr@gmail.com>
2021-01-10 12:03:00 +0200

    Initial commit

---
+++ app/src/main/java/com/pavelrukin/newyorktimes/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.pavelrukin.newyorktimes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pavelrukin.newyorktimes.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
